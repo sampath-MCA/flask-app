3fb10acb37ab1666b82df711f5202b9167bb9620
Sampath Software Engineer <43511705+sampath-MCA@users.noreply.github.com>
2025-09-09 22:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def detect():
 
 @app.route("/upload", methods=["POST"])
 def upload_image():
-    image_data = request.form.get('image')  # Get Base64 string from POST
+    image_data = request.data # Get Base64 string from POST
     if not image_data:
         return "No image data received", 400
 
