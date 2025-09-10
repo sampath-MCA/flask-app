b4f30d27e92c783d9eb0fd0f8c147bce6a707793
Sampath Software Engineer <43511705+sampath-MCA@users.noreply.github.com>
2025-09-10 16:41:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,39 @@ app = Flask(__name__)
 model = YOLO("yolov8n-pose.pt")
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def mergeFile(newFolder,user):
+    folder_path = f"uploads/{newFolder}"
+    combined_base64 = ''
+    
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for file in sorted(filenames, key=lambda x: int(x.split("_")[0])):
+            full_path = os.path.join(dirpath, file)
+            print(full_path)
+            try:
+                with open(full_path, "r") as f:
+                    data = json.load(f)
+                    #print(data)
+                    #print(f"{full_path}")
+                    combined_base64 += data["base"]["image"]
+            except FileNotFoundError:
+                print(f"File not found: {full_path}")
+   
+    return creatImage(combined_base64, newFolder,user,folder_path)
+
+
+def creatImage(base64_string, state,user,folder_path):
+    #print( base64_string)  # Print first 50 chars for preview
+    image_data = base64.b64decode(base64_string)
+    path = f"uploads/{user}/{state}.jpg"
+    
+    with open(path, "wb") as f:
+        f.write(image_data)
+        #shutil.rmtree(folder_path)
+
+
+    return 'hi'
+
 def distance(p1, p2):
     return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
 
@@ -45,11 +78,37 @@ def detect():
         "head_detected": detected
     })
 
-@app.route("/upload", methods=["POST"])
+@app.route('/upload', methods=['POST'])
 def upload_image():
-    print('hi')
     data = request.get_json(silent=True) or {}
-    print(data)
+    user = request.args.get('user')
+    id = request.args.get('id')
+    chunkNo = request.args.get('chunkNo')
+    index = request.args.get('index')
+
+    print(chunkNo)
+
+    data = {
+        "base": data
+    }
+
+    newFolder = f"{user}_{id}"
+    path = os.path.join(UPLOAD_FOLDER, newFolder)
+    os.makedirs(path, exist_ok=True)
+
+    userPath = os.path.join(UPLOAD_FOLDER, user)
+    os.makedirs(userPath, exist_ok=True)
+   
+    # Save JSON file inside the folder
+    filename = f"{index}_{user}_{id}_chunk{chunkNo}.json"
+    #print(filename)
+    filepath = os.path.join(path, filename)
+  
+    
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=4)
+    if(chunkNo == 'last'):
+       return mergeFile(newFolder,user)
     return 'hi'
 
 
